0e27bde5cf20635c560df772b83d95501366b9dd
ndevreeze <nico@ndv.nl>
2024-06-14 18:47:25 +0200

    Bump library version(s) wrt lein ancient

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                  ;; 3.43.0.0 automatically with lein ancient. This
                  ;; removes some generated-id functionality, need to
                  ;; use 'RETURNING' clause, seems a bit more work.
-                 [org.xerial/sqlite-jdbc "3.45.3.0"] ;; SQLite ...
+                 [org.xerial/sqlite-jdbc "3.46.0.0"] ;; SQLite ...
                  [org.postgresql/postgresql "42.7.3"] ;; ... and Postgres for now.
                  [ndevreeze/logger "0.6.2"]
                  ]
