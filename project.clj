23061e4b68e0978f16c0c6ce6eb7ff3c3dd91748
ndevreeze <nico@ndv.nl>
2025-01-09 20:54:19 +0100

    Bump clj versions for flexdb on 2025-01-09

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                  ;; 3.43.0.0 automatically with lein ancient. This
                  ;; removes some generated-id functionality, need to
                  ;; use 'RETURNING' clause, seems a bit more work.
-                 [org.xerial/sqlite-jdbc "3.47.1.0"] ;; SQLite ...
+                 [org.xerial/sqlite-jdbc "3.47.2.0"] ;; SQLite ...
                  [org.postgresql/postgresql "42.7.4"] ;; ... and Postgres for now.
                  [ndevreeze/logger "0.6.2"]
                  ]
