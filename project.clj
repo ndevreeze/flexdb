28be0940cf417a6b30162829aefbda80357c154f
ndevreeze <nico@ndv.nl>
2025-12-24 13:16:03 +0100

    Bump clj versions for flexdb on 2025-12-24

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :url "http://github.com/ndevreeze/flexdb"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.12.3"]
+  :dependencies [[org.clojure/clojure "1.12.4"]
                  ;; [me.raynes/fs "1.4.6"] ;; file system functions
                  [clj-commons/fs "1.6.312"] ;; in place of me.raynes/fs.
                  [clojure.java-time "1.4.3"] ;; new in Java 8, replacing Joda-time
@@ -14,7 +14,7 @@
                  ;; 3.43.0.0 automatically with lein ancient. This
                  ;; removes some generated-id functionality, need to
                  ;; use 'RETURNING' clause, seems a bit more work.
-                 [org.xerial/sqlite-jdbc "3.51.0.0"] ;; SQLite ...
+                 [org.xerial/sqlite-jdbc "3.51.1.0"] ;; SQLite ...
                  [org.postgresql/postgresql "42.7.8"] ;; ... and Postgres for now.
                  [ndevreeze/logger "0.6.2"]
                  ]
